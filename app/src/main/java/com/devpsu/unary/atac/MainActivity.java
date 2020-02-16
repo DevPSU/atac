366db60d524f254447465e05892e445c3c2c102f
Mudit Garg <anilmudit@gmail.com>
2020-02-16 18:22:58 -0500

    first commit

---
+++ app/src/main/java/com/devpsu/unary/atac/MainActivity.java
@@ -0,0 +1,14 @@
+package com.devpsu.unary.atac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
